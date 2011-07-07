31a044edcbecf799eb6d9f4555532b853da34360
maep <bug@me.not>
2011-07-08 00:07:20 +0200

    updated license plates

--- fehlstart.c
+++ fehlstart.c
@@ -1,5 +1,5 @@
 /*
-*   fehlstart - a small launcher written in gnu c99
+*   fehlstart - a small launcher written in c99
 *   this source is published under the GPLv3 license.
 *   get the license from: http://www.gnu.org/licenses/gpl-3.0.txt
 *   copyright 2011 maep and contributors
