d5ae110c6b5dc8455ff402f812a299775ecbc710
mæp <m.aep@live.com>
2013-10-11 18:34:57 +0200

    removed headr

--- src/fehlstart.h
+++
@@ -1,9 +0,0 @@
-/*
-*   fehlstart - a small launcher written in c99
-*   this source is published under the GPLv3 license.
-*   get the license from: http://www.gnu.org/licenses/gpl-3.0.txt
-*   copyright 2013 maep and contributors
-*/
-
-// some typedefs so the xmacros will work
-
